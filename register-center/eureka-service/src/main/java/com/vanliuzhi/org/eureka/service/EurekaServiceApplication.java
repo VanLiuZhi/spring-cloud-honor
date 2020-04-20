70bba15d17d865201439dedc4d916282a04a82cd
VanLiuZhi <1441765847@qq.com>
2020-04-20 23:14:26 +0800

    添加注册中心，网关模块

---
+++ register-center/eureka-service/src/main/java/com/vanliuzhi/org/eureka/service/EurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vanliuzhi.org.eureka.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+}
