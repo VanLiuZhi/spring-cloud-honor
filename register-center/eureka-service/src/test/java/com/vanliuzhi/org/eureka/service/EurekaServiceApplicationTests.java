7d79def514a8b4c93f39d7e52eb959e864887efd
VanLiuZhi <1441765847@qq.com>
2020-04-20 23:27:19 +0800

    eureka 配置

--- register-center/eureka-service/src/test/java/com/vanliuzhi/org/eureka/service/EurekaServiceApplicationTests.java
+++ register-center/eureka-service/src/test/java/com/vanliuzhi/org/eureka/service/EurekaServiceApplicationTests.java
@@ -1,12 +1,10 @@
 package com.vanliuzhi.org.eureka.service;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EurekaServiceApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
