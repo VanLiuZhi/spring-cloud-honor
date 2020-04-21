e9f5c899aa8e7488f3fff0ea858872f7c4836eb6
VanLiuZhi <1441765847@qq.com>
2020-04-21 15:51:06 +0800

    用户中心，数据库配置，mybatis配置，相关代码编写

--- user-center/src/main/java/com/vanliuzhi/org/user/center/dao/SysUserDao.java
+++ user-center/src/main/java/com/vanliuzhi/org/user/center/dao/SysUserDao.java
@@ -1,6 +1,7 @@
 package com.vanliuzhi.org.user.center.dao;
 
 import com.vanliuzhi.org.user.center.entity.SysUser;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
@@ -10,6 +11,7 @@ import java.util.List;
  * @author makejava
  * @since 2020-04-21 09:56:40
  */
+@Mapper
 public interface SysUserDao {
 
     /**
