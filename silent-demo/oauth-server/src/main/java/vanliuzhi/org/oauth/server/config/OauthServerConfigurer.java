544041e990bb7e9c68db0d24dc116a26bc88fbdd
VanLiuZhi <1441765847@qq.com>
2020-08-27 23:06:46 +0800

    silent update code

--- silent-demo/oauth-server/src/main/java/vanliuzhi/org/oauth/server/config/OauthServerConfigurer.java
+++ silent-demo/oauth-server/src/main/java/vanliuzhi/org/oauth/server/config/OauthServerConfigurer.java
@@ -20,7 +20,8 @@ import org.springframework.security.oauth2.provider.token.store.InMemoryTokenSto
 /**
  * 授权服务器配置
  *
- * @author lys3415
+ * @author vanliuzhi
+ * @date 2020/8/27 16:27
  */
 @Configuration
 @EnableAuthorizationServer
@@ -33,22 +34,23 @@ public class OauthServerConfigurer extends AuthorizationServerConfigurerAdapter 
     private AuthenticationManager authenticationManager;
 
     /**
-     * 该对象用来将令牌信息存储到内存中
-     * OAuth2令牌持久化主要有以下几种
+     * 获取用户详情，使用内部实现
+     */
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    /**
+     * 该⽅法⽤于创建tokenStore对象（令牌存储对象）token以什么形式存储
      * <p>
      * 1. InMemoryTokenStore  内存存储 OAuth2默认储存方式
      * 2. JdbcTokenStore  数据库存储
      * 3. RedisTokenStore Redis存储
      * 4. JwkTokenStore & JwtTokenStore
      */
-    @Autowired(required = false)
-    private TokenStore inMemoryTokenStore;
-
-    /**
-     * 获取用户详情
-     */
-    @Autowired
-    private UserDetailsService userDetailsService;
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
 
     /**
      * 指定密码的加密方式
@@ -62,19 +64,27 @@ public class OauthServerConfigurer extends AuthorizationServerConfigurerAdapter 
 
     /**
      * 用来配置令牌端点(Token Endpoint)的安全约束
+     * 这些是框架内部提供的Endpoint，可以配置哪些端点能访问
+     * <p>
+     * /oauth/check_token?token
+     * 通过 get 请求带上token，可以去校验该token
+     * <p>
+     * 其它 Endpoint
+     * /oauth/authorize(授权端，授权码模式使用)
+     * /oauth/token(令牌端，获取 token)
+     * /oauth/confirm_access(用户发送确认授权)
+     * /oauth/error(认证失败)
+     * /oauth/token_key(如果使用JWT，可以获的公钥用于 token 的验签)
+     * <p>
+     * 各个断点作用参考：cnblogs.com/Irving/p/9430460.html
      */
     @Override
-    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        // super.configure(security);
-        //
-        // // 相当于打开endpoints 访问接⼝的开关，这样的话后期我们能够访问该接⼝
-        // security.
-        //         // 允许客户端表单认证
-        //                 allowFormAuthenticationForClients()
-        //         // 开启端⼝/oauth/token_key的访问权限（允许）
-        //         .tokenKeyAccess("permitAll()")
-        //         // 开启端⼝/oauth/check_token的访问权限（允许）
-        //         .checkTokenAccess("permitAll()");
+    public void configure(AuthorizationServerSecurityConfigurer security) {
+        security
+                // 开启端⼝/oauth/token_key的访问权限（允许）
+                .tokenKeyAccess("permitAll()")
+                // 开启端⼝/oauth/check_token的访问权限（允许）
+                .checkTokenAccess("permitAll()");
         // 表示支持 client_id 和 client_secret 做登录认证
         security.allowFormAuthenticationForClients();
     }
@@ -84,34 +94,19 @@ public class OauthServerConfigurer extends AuthorizationServerConfigurerAdapter 
      */
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        // super.configure(clients);
-
-        // clients.
-        //         // 客户端信息存储在什么地⽅，可以在内存中，可以在数据库⾥
-        //                 inMemory()
-        //         // 添加⼀个client配置,指定其client_id
-        //         .withClient("client")
-        //         // 指定客户端的密码/安全码
-        //         .secret("abcxyz")
-        //         // 指定客户端所能访问资源
-        //         .resourceIds("autodeliver")
-        //         // 认证类型/令牌颁发模式，可以配置多个在这⾥，但是不⼀定都⽤，具体使⽤哪种⽅式颁发token，需要客户端调⽤的时候传递参数指定
-        //         .authorizedGrantTypes("password", "refresh_token")
-        //         // 客户端的权限范围，此处配置为all全部即可
-        //         .scopes("all");
-
         // 这里显示配置，客户端是固定的，实际情况最好通过数据库获取客户端信息，方便扩展
         clients.inMemory()
+                // 配置客户端id
                 .withClient("vanliuzhi")
-                //授权模式为password和refresh_token两种
+                // 授权模式为password和refresh_token两种
                 .authorizedGrantTypes("password", "refresh_token")
                 // 配置access_token的过期时间
                 .accessTokenValiditySeconds(1800)
-                //配置资源id
+                // 配置资源id(每个资源服务器都有唯一id，该方法接受的参数也是多个String，可以配置该客户端能访问的资源)
                 .resourceIds("myResource")
                 // 客户端的权限范围，此处配置为all全部即可
                 .scopes("all")
-                //123加密后的密码
+                // 123456加密后的密码(客户端secret)
                 .secret("$2a$10$tnj.nZjSzCBckTh2fRRK9.ZTYfU0y4pDiZZChKxxeOElBsxaQCn26");
     }
 
@@ -132,15 +127,13 @@ public class OauthServerConfigurer extends AuthorizationServerConfigurerAdapter 
         //         .allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
 
         //配置令牌的存储（这里存放在内存中）
-        endpoints.tokenStore(inMemoryTokenStore)
+        endpoints.tokenStore(tokenStore())
                 .authenticationManager(authenticationManager)
                 .userDetailsService(userDetailsService);
     }
 
     /**
      * 该⽅法⽤户获取⼀个token服务对象（该对象描述了token有效期等信息）
-     *
-     * @return
      */
     private AuthorizationServerTokenServices authorizationServerTokenServices() {
 
@@ -160,11 +153,5 @@ public class OauthServerConfigurer extends AuthorizationServerConfigurerAdapter 
 
     }
 
-    /**
-     * 该⽅法⽤于创建tokenStore对象（令牌存储对象）token以什么形式存储
-     */
-    public TokenStore tokenStore() {
-        return new InMemoryTokenStore();
-    }
 }
 
