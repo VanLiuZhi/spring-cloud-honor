1eb033d7db45c7ab1f113560d6077128aa7e560e
lys3415 <ys.liuzhi@h3c.com>
2020-09-27 17:03:20 +0800

    oauth2 demo 工程

---
+++ silent-demo/order-server/src/main/java/vanliuzhi/org/order/server/OrderServerApplication.java
@@ -0,0 +1,19 @@
+package vanliuzhi.org.order.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class OrderServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServerApplication.class, args);
+    }
+
+}
---
+++ silent-demo/user-server/src/main/java/vanliuzhi/org/user/server/UserServerApplication.java
@@ -0,0 +1,19 @@
+package vanliuzhi.org.user.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class UserServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServerApplication.class, args);
+    }
+
+}
