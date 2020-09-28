de255e700d7a4c8c84953fc4ba2a36dd7cdb999b
lys3415 <ys.liuzhi@h3c.com>
2020-09-28 15:42:33 +0800

    add silent-honor

--- silent-honor/auth-center/src/test/java/vanliuzhi/org/auth/center/AuthCenterApplicationTests.java
+++ silent-honor/auth-center/src/test/java/vanliuzhi/org/auth/center/AuthCenterApplicationTests.java
@@ -1,10 +1,10 @@
-package vanliuzhi.org.silent.honor;
+package vanliuzhi.org.auth.center;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SilentHonorApplicationTests {
+class AuthCenterApplicationTests {
 
     @Test
     void contextLoads() {
