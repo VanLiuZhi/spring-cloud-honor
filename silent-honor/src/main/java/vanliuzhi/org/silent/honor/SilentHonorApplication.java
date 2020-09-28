1d5ea5cbcd787ed53b5b02d7df307702e7b1a3c3
lys3415 <ys.liuzhi@h3c.com>
2020-09-28 13:50:07 +0800

    add silent-honor

---
+++ silent-honor/src/main/java/vanliuzhi/org/silent/honor/SilentHonorApplication.java
@@ -0,0 +1,13 @@
+package vanliuzhi.org.silent.honor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilentHonorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilentHonorApplication.class, args);
+    }
+
+}
