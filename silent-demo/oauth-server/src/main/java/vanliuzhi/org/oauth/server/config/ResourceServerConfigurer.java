95a8d9953f8017876a660e0fcd7f7ca90875f743
lys3415 <ys.liuzhi@h3c.com>
2020-09-30 10:45:37 +0800

    update silent-honor

--- silent-demo/oauth-server/src/main/java/vanliuzhi/org/oauth/server/config/ResourceServerConfigurer.java
+++ silent-demo/oauth-server/src/main/java/vanliuzhi/org/oauth/server/config/ResourceServerConfigurer.java
@@ -1,10 +1,17 @@
 package vanliuzhi.org.oauth.server.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.jwt.crypto.sign.MacSigner;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 /**
  * 资源服务器配置
@@ -16,12 +23,16 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 @EnableResourceServer
 public class ResourceServerConfigurer extends ResourceServerConfigurerAdapter {
 
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
         // 配置资源id，这里的资源id和授权服务器中的资源id一致
         resources.resourceId("myResource")
                 // 设置这些资源仅基于令牌认证
                 .stateless(true);
+        resources.tokenStore(new RedisTokenStore(redisConnectionFactory));
     }
 
     /**
@@ -33,6 +44,8 @@ public class ResourceServerConfigurer extends ResourceServerConfigurerAdapter {
                 .antMatchers("/admin/**").hasRole("admin")
                 .antMatchers("/user/**").hasRole("user")
                 .anyRequest().authenticated();
+
+
     }
 
 }
