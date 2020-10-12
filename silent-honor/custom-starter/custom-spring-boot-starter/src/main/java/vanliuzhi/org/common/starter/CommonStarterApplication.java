61cb9dc010c23f0cd1042a0a61750ddc8da70c77
lys3415 <ys.liuzhi@h3c.com>
2020-10-12 18:37:22 +0800

    update

---
+++ silent-honor/custom-starter/custom-spring-boot-starter/src/main/java/vanliuzhi/org/common/starter/CommonStarterApplication.java
@@ -0,0 +1,13 @@
+package vanliuzhi.org.common.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonStarterApplication.class, args);
+    }
+
+}
